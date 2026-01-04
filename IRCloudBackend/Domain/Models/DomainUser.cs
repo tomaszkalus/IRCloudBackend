c2801a9462b0b902a93bc52c34a2343c796f6aed
Tomasz Kalus <tomakal079@student.polsl.pl>
2026-01-04 22:12:28 +0100

    Changed UserProfile entity name to DomainUser

--- IRCloudBackend/Domain/Models/DomainUser.cs
+++ IRCloudBackend/Domain/Models/DomainUser.cs
@@ -2,17 +2,18 @@
 
 namespace IRCloudBackend.Domain.Models;
 
-public class UserProfile
+public class DomainUser
 {
     public int Id { get; set; }
+    public string Username { get; set; } = "";
     public Guid ApplicationUserGuid { get; set; }
     public string? AvatarUrl { get; set; } = "";
     public string? Bio { get; set; } = "";
     public ICollection<Post> CreatedPosts { get; set; } = new List<Post>();
     public ICollection<Review> CreatedReviews { get; set; } = new List<Review>();
     public ICollection<Post> SavedPosts { get; set; } = new List<Post>();
-    public ICollection<UserProfile> FollowedUsers { get; set; } = new List<UserProfile>();
-    public ICollection<UserProfile> FollowingUsers { get; set; } = new List<UserProfile>();
+    public ICollection<DomainUser> FollowedUsers { get; set; } = new List<DomainUser>();
+    public ICollection<DomainUser> FollowingUsers { get; set; } = new List<DomainUser>();
 
     public void UpdateBio(string bio)
     {
