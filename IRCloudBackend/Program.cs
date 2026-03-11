a47bbb2edac650a6ba88432e9de17aeb71b59654
tomaszkalus <tomaszkalus@interia.pl>
2026-03-11 01:40:45 +0100

    Added the Issuer and Audience to the JWT token, extended expiration time

--- IRCloudBackend/Program.cs
+++ IRCloudBackend/Program.cs
@@ -1,13 +1,18 @@
+using System.Text;
+
 using IRCloudBackend.Application.Auth;
 using IRCloudBackend.Application.Services;
+using IRCloudBackend.Application.Users.Login;
 using IRCloudBackend.Application.Users.Register;
 using IRCloudBackend.Infrastructure.DbContexts;
 using IRCloudBackend.Infrastructure.Identity;
 using IRCloudBackend.Infrastructure.Services;
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.IdentityModel.Tokens;
 
 namespace IRCloudBackend
 {
@@ -28,10 +33,34 @@ namespace IRCloudBackend
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = builder.Configuration["Jwt:ValidIssuer"],
+                    ValidAudience = builder.Configuration["Jwt:ValidAudience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(builder.Configuration["Jwt:SecretKey"]!))
+                };
+            });
+
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
             builder.Services.AddScoped<ITokenProvider, TokenProvider>();
+            builder.Services.AddScoped<RefreshTokenProvider>();
             builder.Services.AddScoped<RegisterUser>();
+            builder.Services.AddScoped<LoginUser>();
             builder.Services.AddScoped<CategoryService>();
 
             var app = builder.Build();
